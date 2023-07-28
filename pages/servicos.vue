49d39f05f314ec0b496efdf9bbc159f88c45f84a
Caio <bladellano@gmail.com>
2023-07-28 20:11:21 +0100

    :sparkles:Implementando conceito de rota filho

---
+++ pages/servicos.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div>
+
+    <div class="bg-gray-100">
+      <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+        <nuxt-link :to="{name:'a-empresa'}">A Empresa</nuxt-link>
+        <nuxt-link :to="{name:'servicos'}">Serviços</nuxt-link>
+        <nuxt-link :to="{name:'produtos'}">Produtos</nuxt-link>
+      </div>
+    </div>
+
+    <div>
+      <div class="container mx-auto">
+        <h1 class="text-lg font-bold">Serviços</h1>
+
+        <nuxt-link class="bg-gray-300" to="/servicos/desenvolvimento-de-sites">desenvolvimento-de-sites</nuxt-link>
+        <nuxt-link class="bg-gray-300" to="/servicos/curso-de-vue-js">curso-de-vue-js</nuxt-link>
+
+        <nuxt-child />
+
+      </div>
+    </div>
+
+ </div>
+</template>
+
+<script>
+  export default {
+    name:'',
+    data(){
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- pages/servicos/index.vue
+++ pages/servicos/index.vue
@@ -2,24 +2,7 @@
 
   <div>
 
-    <div class="bg-gray-100">
-      <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
-        <nuxt-link :to="{name:'a-empresa'}">A Empresa</nuxt-link>
-        <nuxt-link :to="{name:'servicos'}">Serviços</nuxt-link>
-        <nuxt-link :to="{name:'produtos'}">Produtos</nuxt-link>
-      </div>
-    </div>
-
-    <div>
-      <div class="container mx-auto">
-        <h1 class="text-lg font-bold">Serviços</h1>
-
-        <nuxt-link to="/servicos/desenvolvimento-de-sites">desenvolvimento-de-sites</nuxt-link>
-        <nuxt-link to="/servicos/curso-de-vue-js">curso-de-vue-js</nuxt-link>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nemo doloremque quaerat odio cupiditate, maxime aperiam voluptates deserunt delectus modi obcaecati quo tempora a, at eveniet! Eligendi omnis maiores magnam? Minus praesentium recusandae incidunt blanditiis, accusamus ab saepe ullam totam unde earum adipisci enim cum amet voluptas doloremque commodi fugit?</p>
-      </div>
-    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quod similique deleniti beatae quis, tenetur corrupti dolores hic unde voluptates eos exercitationem quisquam a aliquid amet laudantium alias! Rerum repellendus dolore odit quo reprehenderit adipisci maiores quia magnam, cum beatae provident eius autem facilis fugit.</p>
 
  </div>
 </template>
