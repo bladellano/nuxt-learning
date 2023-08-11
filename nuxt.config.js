8a0dc6b05ed1646e8c75e0fd7d26bd13530a0316
Caio <bladellano@gmail.com>
2023-08-11 20:25:40 +0100

    :sparkles:Adicionando interceptadores axios e mudando URL_API para .env

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,7 @@ export default {
     },
     '@/plugins/dayjs',
     '@/plugins/vtooltip',
+    '@/plugins/axios'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -60,7 +61,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://swapi.dev/api/',
+    baseURL: '',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
